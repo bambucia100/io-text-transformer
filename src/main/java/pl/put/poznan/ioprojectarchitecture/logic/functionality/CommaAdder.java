e14b6d356b2fc01a74d28b3e15487aa479116f5f
rochusqwerty <rochus96@gmail.com>
2018-11-19 18:25:18 +0100

    add CommaAdder functionality

--- src/main/java/pl/put/poznan/ioprojectarchitecture/logic/functionality/CommaAdder.java
+++ src/main/java/pl/put/poznan/ioprojectarchitecture/logic/functionality/CommaAdder.java
@@ -1,4 +1,5 @@
 package pl.put.poznan.ioprojectarchitecture.logic.functionality;
+import java.util.regex.*;
 
 public class CommaAdder {
 
@@ -18,7 +19,16 @@ public class CommaAdder {
 	//TODO Add functionality
 	public String addComma() {
 		if(comma) {
-			
+			String[] arr = text.split(" ");
+			char[] chars;
+			text = arr[0];
+			for(int i = 1; i < arr.length; i++)
+				if(Pattern.matches("[A-Z].+", arr[i]) && !Pattern.matches(".+\\.$", arr[i-1]))
+					text +=  ". " + arr[i];
+				else
+					text +=  " " + arr[i];
+			if(!Pattern.matches(".+\\.$", arr[arr.length-1]))
+			text +=  ".";
 			return text;
 		} else {
 			
