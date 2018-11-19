b5ad08b9ff9515dfa6ddfbfb34749e1240235c4d
rochusqwerty <rochus96@gmail.com>
2018-11-19 18:25:45 +0100

    add Leetspeak functionality

--- src/main/java/pl/put/poznan/ioprojectarchitecture/logic/functionality/Leetspeak.java
+++ src/main/java/pl/put/poznan/ioprojectarchitecture/logic/functionality/Leetspeak.java
@@ -14,12 +14,15 @@ public class Leetspeak {
 		this.leetspeak = leetspeak;
 		this.text = text;
 	}
-	
-	//TODO: Add functionality
+
+	private static String[] normal = {"a", "A", "b", "B", "e", "E", "g", "G", "i", "I", "o", "O", "s", "S", "t", "T", "z", "Z"};
+	private static String[] leet = {"4", "4", "8", "8", "3", "3", "9", "9", "1", "1", "0", "0", "5", "5", "7", "7", "2", "2"};
+
 	public String make1337() {
 		
 		if(leetspeak) {
-			
+			for(int i = 0; i < normal.length; i++)
+				text = text.replaceAll(normal[i], leet[i]);
 			return text;
 		} else {
 			
